db98fc54699861e2c219ad02627a8604f4d9f7fc
Mukunth Arjunan <mukunth.arjunan@gmail.com>
2023-11-13 20:13:26 -0500

    adding intial files and models

---
+++ models/index.js
@@ -0,0 +1,37 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id"
+});
+
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey:"category_id"
+})
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through:{
+    model:ProductTag
+  }
+})
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through:{
+    model:ProductTag
+  }
+})
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
---
+++ server.js
@@ -0,0 +1,20 @@
+// Import express
+const express = require("express");
+// Import routes files
+const routes = require("./routes");
+// Import sequelize connection
+const sequelize = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+});
